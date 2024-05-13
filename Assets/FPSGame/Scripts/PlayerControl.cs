8ef2513d37dfa53d08e7d9ec7e905562f2426b18
Dophian <169108628+Dophian@users.noreply.github.com>
2024-05-13 15:34:29 +0900

    플레이어 Idle-Run 애니메이션 전환 로직 추가
    - Animator Controller 구성
    - PlayerControl에서 애님 상태 설정

--- Assets/FPSGame/Scripts/PlayerControl.cs
+++ Assets/FPSGame/Scripts/PlayerControl.cs
@@ -6,7 +6,10 @@ namespace FPSGame
     {
         // 이동 속도.
         [SerializeField] private float moveSpeed = 5f;
-        
+
+        // Animator 컴포넌트 변수.
+        [SerializeField] private Animator refAnimator;
+
         // 트랜스폼 컴포넌트 참조 변수.
         private Transform refTransform;
 
@@ -14,6 +17,9 @@ namespace FPSGame
         {
             // 트랜스폼 참조 저장.
             refTransform = transform;
+
+            // 참조 저장.
+            refAnimator = GetComponentInChildren<Animator>();
         }
 
         private void Update()
@@ -21,6 +27,18 @@ namespace FPSGame
             // 방향키 입력 받기.
             float horizontal = Input.GetAxis("Horizontal");
             float vertical = Input.GetAxis("Vertical");
+
+            // 애니메이션 설정.
+            if (horizontal == 0f && vertical == 0f)
+            {
+                // 입력이 없음.
+                refAnimator.SetInteger("State", 0);
+            }
+            else
+            {
+                // 입력이 있음.
+                refAnimator.SetInteger("State", 1);
+            }
 
             // 이동.
             refTransform.position +=
